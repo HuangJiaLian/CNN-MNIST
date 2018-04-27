56453faf861d62deeff9aea6208a814d5f2d0a12
Jack Huang <719668276@qq.com>
2018-04-27 21:56:51 +0800

    优化用户体验

--- src/cnn_app.py
+++ src/cnn_app.py
@@ -15,17 +15,21 @@ import os
 MODEL_SAVE_PATH = './model/'
 MODEL_NAME='cnn_mnist_model' 
 
+Chanel = 3
 PaintSize = 350
-BgColor = 255
-PaintColor = 0
+# BgColor = 255
+BgColor = (255,255,255)
+# PaintColor = 0
+PaintColor = (0,0,0)
 StrokeWeight = 20
 
 drawing = False
 start = (-1, -1)
 lastPoint = (-1,-1)
 
+
 # 初始化画板背景色为白色
-img = np.full((PaintSize, PaintSize,1), BgColor, dtype=np.uint8)
+img = np.full((PaintSize, PaintSize,Chanel), BgColor, dtype=np.uint8)
 
 # 有鼠标事件就会调用下面这个函数画图
 def mouse_event(event, x, y, flags, param):
@@ -49,7 +53,7 @@ def mouse_event(event, x, y, flags, param):
 def pre_pic(picName):
     img = Image.open(picName)
     reIm = img.resize((28,28), True) # 将1D存储的数据改变成2D
-    im_arr = np.array(reIm)  # 将PIL.Image.Image格式转化为numpy.ndarray方便显示
+    im_arr = np.array(reIm.convert('L'))  # 将PIL.Image.Image格式转化为numpy.ndarray方便显示
     # cv2.imshow("Little",im_arr) # 显示缩小后的图像
     
     # 由于MNIST中1对应纯黑，0对应纯白
@@ -173,11 +177,18 @@ def restore_model(testPicArr):
 # 应用程序
 def application():
     global img
-    cv2.namedWindow('Press \'s\' to Save,\'c\' to clear')
-    cv2.setMouseCallback('Press \'s\' to Save,\'c\' to clear', mouse_event)
+    # Text info
+    font  = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (10,30)
+    fontScale  = 1
+    fontColor  = (0,255,0)
+    lineType  = 2
+
+    cv2.namedWindow('Press \'s\' to Save,\'c\' to Clear')
+    cv2.setMouseCallback('Press \'s\' to Save,\'c\' to Clear', mouse_event)
     print("Press q or Esc to quit the program:")
     while True:
-        cv2.imshow('Press \'s\' to Save,\'c\' to clear', img)
+        cv2.imshow('Press \'s\' to Save,\'c\' to Clear', img)
         key = cv2.waitKey(20)
         if key == 27 or key == 113: # 按`q`或者`Esc`退出
             break
@@ -187,9 +198,12 @@ def application():
             print(imgName + " saved")
             testPicArr = pre_pic(imgName)
             preValue = restore_model(testPicArr)
-            print ("The prediction number is:", preValue)
+            cv2.putText(img,'I think it\'s ' + str(preValue[0]) + '.', bottomLeftCornerOfText, font,  fontScale, fontColor,lineType)
+            imgName = './pic/' + str(preValue[0]) +'.png'
+            cv2.imwrite(imgName, img)
+            print(imgName + " saved")
         elif key == 99: # 按`c`清空画图板
-            img = np.full((PaintSize, PaintSize,1), BgColor, dtype=np.uint8)
+            img = np.full((PaintSize, PaintSize,Chanel), BgColor, dtype=np.uint8)
         else:
             pass
 
